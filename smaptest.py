fa2ef3f757e273f8ce1764f8e2e493ab0d94f6d2
Sean Knight <sean@thatsean.com>
2023-03-29 17:45:13 -0700

    added sf polygons

--- smaptest.py
+++ smaptest.py
@@ -1,8 +1,31 @@
 import streamlit as st
 import folium
+import geopandas as gpd
 
-def create_map(lat,lon,zoom):
-    m = folium.Map(location=[lat,lon], zoom_start=zoom)
+# Load the Shapefile
+gdf = gpd.read_file("geo_export_844c2f95-cd14-43b0-b47a-8613f7501b2e.shp")
+
+# Function to define the style of the polygons
+def style_function(feature):
+    return {
+        'fillColor': '#3186cc',
+        'color': 'black',
+        'weight': 2,
+        'fillOpacity': 0.7
+    }
+
+def create_map(gdf, lat,lon,zoom, style_function):
+    m = folium.Map(
+        location=[lat,lon], 
+        zoom_start=zoom
+    )
+
+    # Add the shapefile overlay to the map using GeoJson
+    folium.GeoJson(
+        gdf,
+        style_function=style_function,
+        tooltip = folium.GeoJsonTooltip(fields=['name'], aliases=['name'])
+        ).add_to(m)
     return m
 
 st.title("My Map App Built with Streamlit and Folium")
@@ -11,7 +34,7 @@ lat = st.number_input("Latitude", value = 37.79048192546293)
 lon = st.number_input("Longitude", value = -122.3996870713028)
 zoom = st.slider("Zoom Level", 1, 20, 10)
 
-map = create_map(lat,lon,zoom)
+map = create_map(gdf,lat,lon,zoom, style_function)
 
 st.write(map._repr_html_(), unsafe_allow_html=True)
 
